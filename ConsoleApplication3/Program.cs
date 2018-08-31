013e93cfed6518e0017829da7737620bd6b23bb2
Justin L Wilson <jlwilson@forsythco.com>
2018-08-31 09:38:28 -0400

    Add project files.

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftGeom
+{
+    class Program
+    {
+        private const double EarthRadius = 6378137;
+        private const double MinLatitude = -85.05112878;
+        private const double MaxLatitude = 85.05112878;
+        private const double MinLongitude = -180;
+        private const double MaxLongitude = 180;
+        static void Main(string[] args)
+        {
+
+            double lat, lon, mLat, mLon;
+
+            int t1x = 139552, t1y = 209303;
+            int t2x = 139555, t2y = 209305;
+
+            TileXYToPixelXY(t1x, t1y, out t1x, out t1y);
+            TileXYToPixelXY(t2x, t2y, out t2x, out t2y);
+
+            PixelXYToLatLong(t1x, t1y, 19, out lat, out lon);
+            PixelXYToLatLong(t2x + 256, t2y + 256, 19, out mLat, out mLon);
+            Console.WriteLine(String.Format("{0}, {1}, {2}, {3}", lon, lat, mLon, mLat));
+            Console.ReadLine();
+        }
+
+        public static void PixelXYToTileXY(int pixelX, int pixelY, out int tileX, out int tileY)
+        {
+            tileX = pixelX / 256;
+            tileY = pixelY / 256;
+        }
+
+        public static void TileXYToPixelXY(int tileX, int tileY, out int pixelX, out int pixelY)
+        {
+            pixelX = tileX * 256;
+            pixelY = tileY * 256;
+        }
+
+        public static void PixelXYToLatLong(int pixelX, int pixelY, int levelOfDetail, out double latitude, out double longitude)
+        {
+            double mapSize = MapSize(levelOfDetail);
+            double x = (Clip(pixelX, 0, mapSize - 1) / mapSize) - 0.5;
+            double y = 0.5 - (Clip(pixelY, 0, mapSize - 1) / mapSize);
+
+            latitude = 90 - 360 * Math.Atan(Math.Exp(-y * 2 * Math.PI)) / Math.PI;
+            longitude = 360 * x;
+        }
+
+        public static uint MapSize(int levelOfDetail)
+        {
+            return (uint)256 << levelOfDetail;
+        }
+
+        public static double MapScale(double latitude, int levelOfDetail, int screenDpi)
+        {
+            return GroundResolution(latitude, levelOfDetail) * screenDpi / 0.0254;
+        }
+
+        public static double GroundResolution(double latitude, int levelOfDetail)
+        {
+            latitude = Clip(latitude, MinLatitude, MaxLatitude);
+            return Math.Cos(latitude * Math.PI / 180) * 2 * Math.PI * EarthRadius / MapSize(levelOfDetail);
+        }
+
+        private static double Clip(double n, double minValue, double maxValue)
+        {
+            return Math.Min(Math.Max(n, minValue), maxValue);
+        }
+
+    }
+}
